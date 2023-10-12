a5cd47cdd7516cf969bd21d03839f39fc288c204
ReyNicolas <rey.nicolas.92@gmail.com>
2023-10-12 15:32:27 -0300

    Fixed container bug

---
+++ Assets/Scripts/Player/ShipLogic/CollectorLogic/ContainerRotate.cs
@@ -0,0 +1,43 @@
+using UnityEngine;
+using static UnityEngine.InputSystem.InputAction;
+using System;
+
+public class ContainerRotate : MonoBehaviour
+{
+    [SerializeField] int degreesPerSecond;
+    [SerializeField] AudioSource audioSource;
+    [SerializeField] AudioClip audioClip;
+    [SerializeField] float degrees;
+
+    private void Update()
+    {
+        if (degrees != 0) Rotate();
+    }
+
+    public void RotateRight(CallbackContext context)
+    {
+        if (context.started) AddRotation( -60 );
+    }
+
+  
+    public void RotateLeft(CallbackContext context)
+    {
+        if (context.started) AddRotation(60);
+    }
+    void AddRotation(float degrees)
+    {
+        audioSource.PlayOneShot(audioClip, 0.2f);
+        this.degrees += degrees;
+    }
+
+
+    void Rotate()
+    {
+        float degreesDelta = (degrees > 0)
+            ? MathF.Min(degrees, degreesPerSecond * Time.deltaTime)
+            : Math.Max(degrees, -degreesPerSecond * Time.deltaTime);
+        transform.Rotate(Vector3.forward * degreesDelta);
+        degrees -= degreesDelta;
+        return;  
+    }
+}
